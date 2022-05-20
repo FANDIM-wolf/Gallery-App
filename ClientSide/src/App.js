2da78f71c82569a5ae9100305305bd26e4ec7747
Mikhail Yolkin <63235817+FANDIM-wolf@users.noreply.github.com>
2022-05-20 19:33:59 +0300

    Add files via upload

--- ClientSide/src/App.js
+++ ClientSide/src/App.js
@@ -3,9 +3,12 @@ import './App.css';
 
 import Header from './components/header';
 import ImageUpload from './components/ImageUpload';
-import {BrowserRouter,Route,Routes,Switch} from 'react-router-dom'
+import {BrowserRouter,Route,Routes} from 'react-router-dom'
 import Login from './components/login';
 import AddUser from './components/add_user';
+import ViewUser from './components/view_user';
+import EditUser from './components/edit_user';
+import ViewAnImage from './components/view_an_image';
 function App() {
   return (
     <BrowserRouter>
@@ -15,10 +18,16 @@ function App() {
     
     <Route exact path="/login" element={<Login/>}>
      </Route>
-     <Route exact path="/adduser" element={<AddUser/>}>
+     <Route exact path="users/add-user" element={<AddUser/>}>
      </Route>  
+     <Route exact path="users/edit-user/:id" element={<EditUser/>}>
+     </Route>
+     <Route exact path="/users" element={<ViewUser/>}>
+     </Route>
     <Route path="/upload-image" element={ <ImageUpload/> }>
-      </Route>
+    </Route>
+      <Route path="/view-image/:id" element={<ViewAnImage/>}></Route>
+
    
     
     
