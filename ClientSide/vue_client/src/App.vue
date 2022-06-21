4106cceff5b5433c40b0d9e949ce4ab7c4b89b99
Mikhail Shishov <63235817+FANDIM-wolf@users.noreply.github.com>
2022-06-21 19:32:49 +0300

    Add files via upload

--- ClientSide/vue_client/src/App.vue
+++ ClientSide/vue_client/src/App.vue
@@ -1,58 +1,12 @@
 <template>
-<div>
-  <button @click="showDialog">
-    Создать пост
-  </button>
-  <MyDialog v-model:show="dialogVisible" >
-    <PostForm @create="createPost" />
-  </MyDialog>
- 
-  <PostList v-bind:posts="posts" @remove="removePost" />
-  </div>
+
+ <router-view>
+
+ </router-view>
 </template>
 
 <script>
-import PostForm from './components/PostForm.vue'
-import PostList from './components/PostList.vue'
-import MyDialog from './components/UI/MyDialog.vue'
-export default {
-  name: 'App',
-  components: {
-   
-    PostForm,
-    PostList,
-    MyDialog
-  },
-   data(){
-    return{
-       posts: [
-            { id:1, Name: 'Frank', Description: 'Murphy' },
-           
-        ],
-        dialogVisible:false
-     
-    }
-  },
-  methods:{
-    createPost(post){
-      console.log(post);
-     
-      
-      this.posts.push(post);
-      
-    },
 
-    removePost(post){
-      this.posts= this.posts.filter( p => p.id !== post.id)
-    }
-    ,
-    showDialog(){
-        this.dialogVisible = true;
-    }
-    
-  }
- 
-}
 </script>
 
 <style>
---
+++ ClientSide/vue_client/src/components/PostView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>POST</h1>
+    <p>view post</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
