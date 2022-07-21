48958815b7dc1963a634ebd7e08814106ce655d0
Mikhail Shishov <63235817+FANDIM-wolf@users.noreply.github.com>
2022-07-21 13:56:01 +0300

    Add files via upload

---
+++ app/src/src/main/java/com/example/api_gallery_app/ApiGalleryAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.api_gallery_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGalleryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGalleryAppApplication.class, args);
+	}
+
+}
---
+++ app/src/src/main/java/com/example/api_gallery_app/controllers/JDBController.java
@@ -0,0 +1,12 @@
+package com.example.api_gallery_app.controllers;
+
+import com.example.api_gallery_app.entity.UserEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCallback;
+
+import java.sql.PreparedStatement;
+
+public class JDBController {
+
+}
