da15365297eace922d545389d920ec08de326c2e
Mikhail Shishov <63235817+FANDIM-wolf@users.noreply.github.com>
2022-07-23 17:25:58 +0300

    Add files via upload

---
+++ app/main/java/com/example/api_gallery_app/repository/UserRepo.java
@@ -0,0 +1,21 @@
+package com.example.api_gallery_app.repository;
+
+import com.example.api_gallery_app.entity.UserEntity;
+import org.apache.catalina.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepo extends CrudRepository<UserEntity ,Long> {
+
+
+
+}
