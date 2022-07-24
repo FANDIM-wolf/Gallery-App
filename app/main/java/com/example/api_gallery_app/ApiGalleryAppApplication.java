647a9a2441e4bc819a0c33543168a3ac838eca90
Mikhail Shishov <63235817+FANDIM-wolf@users.noreply.github.com>
2022-07-24 14:46:27 +0300

    Add files via upload

--- app/main/java/com/example/api_gallery_app/ApiGalleryAppApplication.java
+++ app/main/java/com/example/api_gallery_app/ApiGalleryAppApplication.java
@@ -1,9 +1,14 @@
 package com.example.api_gallery_app;
 
+import com.example.api_gallery_app.controllers.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+		FileStorageProperties.class
+})
 public class ApiGalleryAppApplication {
 
 	public static void main(String[] args) {
--- app/main/java/com/example/api_gallery_app/controllers/UserController.java
+++ app/main/java/com/example/api_gallery_app/controllers/UserController.java
@@ -2,31 +2,43 @@ package com.example.api_gallery_app.controllers;
 import com.example.api_gallery_app.entity.UserEntity;
 import com.example.api_gallery_app.exeception.UserAlreadyExistException;
 import com.example.api_gallery_app.repository.UserRepo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import org.springframework.core.io.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.io.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import com.example.api_gallery_app.service.FileStorageService;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
-
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+    @Autowired
+    private FileStorageService fileStorageService;
     @Autowired
     private UserRepo userRepo;
+
     @GetMapping("/all")
-    public String greeting(){
+    public String greeting() {
         return "/users";
     }
+
     @PostMapping("/save")
     public ResponseEntity save(@RequestBody UserEntity user) throws UserAlreadyExistException {
         //check for existing
@@ -61,7 +73,7 @@ public class UserController {
     }
 
     public static void printSQLException(SQLException ex) {
-        for (Throwable e: ex) {
+        for (Throwable e : ex) {
             if (e instanceof SQLException) {
                 e.printStackTrace(System.err);
                 System.err.println("SQLState: " + ((SQLException) e).getSQLState());
@@ -75,21 +87,70 @@ public class UserController {
             }
         }
     }
+
     //return Data about certain user
     @GetMapping("/get_user")
-    public Optional<UserEntity> get_user_by_Id(@RequestParam  Long id){
+    public Optional<UserEntity> get_user_by_Id(@RequestParam Long id) {
 
         return userRepo.findById(id);
     }
 
     //upload  file
+
     @PostMapping("/upload")
     public String fileUpload(@RequestParam("file") MultipartFile file) throws IOException {
-        File convertFile = new File("D:/"+file.getOriginalFilename());
+        File convertFile = new File("D:/" + file.getOriginalFilename());
         convertFile.createNewFile();
         FileOutputStream fout = new FileOutputStream(convertFile);
         fout.write(file.getBytes());
         fout.close();
         return "File is upload successfully";
     }
+
+    @PostMapping("/uploadFile")
+    public UploadFileResponse uploadFile(@RequestParam("file") MultipartFile file) throws FileStorageException {
+        String fileName = fileStorageService.storeFile(file);
+
+        String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/downloadFile/")
+                .path(fileName)
+                .toUriString();
+
+        return new UploadFileResponse(fileName, fileDownloadUri,
+                file.getContentType(), file.getSize());
+    }
+
+    @PostMapping("/uploadMultipleFiles")
+    public List<UploadFileResponse> uploadMultipleFiles(@RequestParam("files") MultipartFile[] files) {
+        return Arrays.asList(files)
+                .stream()
+                .map(file -> uploadFile(file))
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/downloadFile/{fileName:.+}")
+    public ResponseEntity<Resource> downloadFile(@PathVariable String fileName, HttpServletRequest request) {
+        // Load file as Resource
+        Resource resource = (Resource) fileStorageService.loadFileAsResource(fileName);
+
+        // Try to determine file's content type
+        String contentType = null;
+        try {
+            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+        } catch (IOException ex) {
+            logger.info("Could not determine file type.");
+        }
+
+        // Fallback to the default content type if type could not be determined
+        if(contentType == null) {
+            contentType = "application/octet-stream";
+        }
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
+    }
+
+
 }
